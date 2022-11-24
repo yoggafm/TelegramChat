97c5227480896fcbf7acfcdf5327dbe30dae48ae
yoggafm <118955791+yoggafm@users.noreply.github.com>
2022-11-24 12:34:10 +0700

    setting token
    setting token

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", "5609275386:AAEZyazPN9u8ai9G5Kg4xuUvR9UTgfY0y3k") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "JualVIPVVIP") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "Findjodohonline") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "Kebugaran2023") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "")
