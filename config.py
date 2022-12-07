cdb39e4cabc15f81dd61035471157931e2d41107
yoggafm <118955791+yoggafm@users.noreply.github.com>
2022-12-07 10:31:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5609275386:AAEZyazPN9u8ai9G5Kg4xuUvR9UTgfY0y3k") # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "JualVIPVVIP") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "Findjodohonline") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "Kebugaran2023") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", " ") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", " ") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", " ") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", " ") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "")
